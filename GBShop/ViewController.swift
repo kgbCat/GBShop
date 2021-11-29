5932a40ccce9049df71209598a958d5250c90437
kgbCat <delova.anna@icloud.com>
2021-11-29 13:41:16 +1200

    Initial

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Anna Delova on 11/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
