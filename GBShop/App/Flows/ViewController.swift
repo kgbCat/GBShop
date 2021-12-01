01adc2a102ddea3442cc472bae4864361f64bad5
kgbCat <delova.anna@icloud.com>
2021-12-01 18:53:30 +1200

    info.plist recover

---
+++ GBShop/App/Flows/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Anna Delova on 12/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
